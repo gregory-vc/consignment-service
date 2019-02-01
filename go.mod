40ab49d3c8392a68a863a9e0da862838d4bc5fbc
Gregory <its.crowd@gmail.com>
2019-02-01 18:57:36 +0300

    fix

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/gregory-vc/consignment-service
 require (
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8 // indirect
 	github.com/golang/protobuf v1.2.1-0.20190109072247-347cf4a86c1c
-	github.com/gregory-vc/vessel-service v0.0.0-20190131143940-3effc60e3a1e
+	github.com/gregory-vc/vessel-service v0.0.0-20190201155718-0400b3b17612
 	github.com/micro/go-micro v0.24.0
 	golang.org/x/net v0.0.0-20190125091013-d26f9f9a57f3
 	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
--- proto/consignment/consignment.pb.go
+++ proto/consignment/consignment.pb.go
@@ -9,6 +9,12 @@ import (
 	math "math"
 )
 
+import (
+	client "github.com/micro/go-micro/client"
+	server "github.com/micro/go-micro/server"
+	context "golang.org/x/net/context"
+)
+
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
 var _ = fmt.Errorf
@@ -275,4 +281,77 @@ var fileDescriptor_e5e5ab05dfa973d5 = []byte{
 	0xff, 0x31, 0x9c, 0x36, 0xfd, 0x32, 0x39, 0x7e, 0xad, 0xdd, 0x50, 0xf2, 0x8f, 0x3c, 0xc0, 0x64,
 	0x81, 0xd6, 0xe3, 0x0c, 0xb9, 0x39, 0x0e, 0x1e, 0x36, 0x7d, 0xda, 0xf8, 0xe7, 0x81, 0x7b, 0xe9,
 	0xf7, 0x5f, 0x01, 0x00, 0x00, 0xff, 0xff, 0x86, 0x3d, 0x82, 0x7e, 0x10, 0x03, 0x00, 0x00,
+}
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ client.Option
+var _ server.Option
+
+// Client API for ShippingService service
+
+type ShippingServiceClient interface {
+	CreateConsignment(ctx context.Context, in *Consignment, opts ...client.CallOption) (*Response, error)
+	GetConsignments(ctx context.Context, in *GetRequest, opts ...client.CallOption) (*Response, error)
+}
+
+type shippingServiceClient struct {
+	c           client.Client
+	serviceName string
+}
+
+func NewShippingServiceClient(serviceName string, c client.Client) ShippingServiceClient {
+	if c == nil {
+		c = client.NewClient()
+	}
+	if len(serviceName) == 0 {
+		serviceName = "go.micro.srv.consignment"
+	}
+	return &shippingServiceClient{
+		c:           c,
+		serviceName: serviceName,
+	}
+}
+
+func (c *shippingServiceClient) CreateConsignment(ctx context.Context, in *Consignment, opts ...client.CallOption) (*Response, error) {
+	req := c.c.NewRequest(c.serviceName, "ShippingService.CreateConsignment", in)
+	out := new(Response)
+	err := c.c.Call(ctx, req, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *shippingServiceClient) GetConsignments(ctx context.Context, in *GetRequest, opts ...client.CallOption) (*Response, error) {
+	req := c.c.NewRequest(c.serviceName, "ShippingService.GetConsignments", in)
+	out := new(Response)
+	err := c.c.Call(ctx, req, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// Server API for ShippingService service
+
+type ShippingServiceHandler interface {
+	CreateConsignment(context.Context, *Consignment, *Response) error
+	GetConsignments(context.Context, *GetRequest, *Response) error
+}
+
+func RegisterShippingServiceHandler(s server.Server, hdlr ShippingServiceHandler, opts ...server.HandlerOption) {
+	s.Handle(s.NewHandler(&ShippingService{hdlr}, opts...))
+}
+
+type ShippingService struct {
+	ShippingServiceHandler
+}
+
+func (h *ShippingService) CreateConsignment(ctx context.Context, in *Consignment, out *Response) error {
+	return h.ShippingServiceHandler.CreateConsignment(ctx, in, out)
+}
+
+func (h *ShippingService) GetConsignments(ctx context.Context, in *GetRequest, out *Response) error {
+	return h.ShippingServiceHandler.GetConsignments(ctx, in, out)
 }
